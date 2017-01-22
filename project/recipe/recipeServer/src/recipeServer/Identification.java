80e026bd5082db0b2ad79392b47c5bd66f157974
정영호 <jyh1536@naver.com>
2017-01-22 22:26:04 +0900

    폴더 정리

---
+++ project/recipe/recipeServer/src/recipeServer/Identification.java
@@ -0,0 +1,18 @@
+package recipeServer;
+
+public class Identification {
+
+	private String dbIp="121.181.123.16:1521:orcl";
+	private String dbId="jyh";
+	private String dbPassword="0911";
+	private String mailId="jyh050211@gmail.com";
+	private String mailPW="wjd091123";
+	
+	public String getdbIP(){return dbIp;}
+	public String getdbId(){return dbId;}
+	public String getdbPassword(){return dbPassword;}
+	public String getmailId(){return mailId;}
+	public String getmailPW(){return mailPW;}
+	
+	
+}
